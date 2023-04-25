eec0514c6980fd2aff5866a5ef67e31ec4efb4dd
Vyacheslav <52282118+SlavikArt@users.noreply.github.com>
2023-04-25 22:53:50 +0300

    Добавьте файлы проекта.

---
+++ README.md
@@ -0,0 +1,13 @@
+
+# Homework_24.04.2023
+Д/з: vector
+
+<a href="https://gist.github.com/SlavikArt/e3980ae773f96e7a75a467ea279638de">Gist - страница со всеми кодами проектов</a>
+
+* [main](main)
+
+<p align="center">
+    <h2>Main</h2>
+    <p></p>
+    <img src="images/main.png">
+</p>
---
+++ main/main.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <vector>
+#include "Point.h"
+using namespace std;
+
+int main()
+{
+	srand(time(0));
+
+	cout << "vector<Point<int>>:\n";
+
+	vector<Point<int>> vec;
+
+	for (int i = 0; i < 5; i++)
+	{
+		vec.push_back(
+			Point<int>(
+				rand() % 10, 
+				rand() % 10, 
+				rand() % 10
+			)
+		);
+	}
+
+	for (int i = 0; i < vec.size(); i++)
+	{
+		cout << vec[i] << "\n";
+	}
+	cout << "\n";
+
+	cout << "vector<vector<Point<int>>>:\n";
+
+	vector<vector<Point<int>>> vec2;
+
+	for (int i = 0; i < 3; i++)
+	{
+		vector<Point<int>> tmp;
+
+		for (int i = 0; i < 5; i++)
+		{
+			tmp.push_back(
+				Point<int>(
+					rand() % 10,
+					rand() % 10,
+					rand() % 10
+				)
+			);
+		}
+		vec2.push_back(tmp);
+	}
+
+	for (int i = 0; i < vec2.size(); i++)
+	{
+		for (int j = 0; j < vec2[i].size(); j++)
+		{
+			cout << vec2[i][j] << ", ";
+		}
+		cout << "\n";
+	}
+}
